df567036427b8c63507ee9a6f7a12c412f516f09
Dodoslav Novak <dodoslavn@gmail.com>
2024-07-22 21:50:15 +0200

    title

--- web/login/index.php
+++ web/login/index.php
@@ -57,7 +57,7 @@ if ( isset($_POST) )
 <div class="wrap-login100 p-t-50 p-b-90">
 <form class="login100-form validate-form flex-sb flex-w" action="" method="post">
 <span class="login100-form-title p-b-51">
-PhpWakeupOnLan
+PHP Wakeup On Lan
 </span>
 <div class="wrap-input100 validate-input m-b-16" data-validate="Username is required">
 <input class="input100" type="text" name="username" placeholder="Username">
