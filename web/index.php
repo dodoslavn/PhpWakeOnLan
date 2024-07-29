f35673e54b9d6d0abc9f6b047e38fc981a902d36
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-07-29 14:44:34 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -69,6 +69,6 @@ else
  
 ?>
    </div>
-   <div id="footer"> <a href="https://github.com/dodoslavn/PhpWakeOnLan/">Github</a> </div>
+   <div id="footer"> PhpWakeOnLan : Dodoslav Novak : <a style="color: rgb(50, 168, 82);" href="https://github.com/dodoslavn/PhpWakeOnLan/">Github</a> </div>
   </body>
 </html>
