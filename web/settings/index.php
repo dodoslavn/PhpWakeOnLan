f37376a0be351a036aaaa720981f1d9293896119
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-07-29 14:14:12 +0200

    Update index.php

--- web/settings/index.php
+++ web/settings/index.php
@@ -25,11 +25,14 @@ if (empty($_SESSION['id'])) header('Location: login/');
     <link rel="stylesheet" type="text/css" href="/features/main_page.css">
   </head>
   <body>
-    <div id="header">
+  <div id="header">
+    <div id="title">PhpWakeOnLan</div>
+    <div id="links">
       <a href="/arp/">ARP</a>
       <a href="/settings/">SETTINGS</a>
       <a href="/logout.php">LOGOUT</a>
     </div>
+  </div>
     <div id="content">
       <h4>Your account</h4>
       <table>
