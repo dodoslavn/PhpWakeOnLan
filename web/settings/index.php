a6a0ff1e1d814d020cb85d738019e56b7da745a9
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-07-29 12:30:41 +0200

    Update index.php

--- web/settings/index.php
+++ web/settings/index.php
@@ -1,5 +1,15 @@
 <?
+session_start();
 
+$config_file_raw = file_get_contents('../../config.json');
+$config = json_decode($config_file_raw);
+if (empty($config)) die("failed to parse JSON config");
+
+# set default, many ways to authenticate
+$auth = false;
+
+# authenticate via logging in, user+pass 
+if (isset($_SESSION['id'])) $auth = true; 
 
 
 ?>
