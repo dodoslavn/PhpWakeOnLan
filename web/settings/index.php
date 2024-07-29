60675dd88dee8007552eebe1e9689a956650c498
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-07-29 14:51:44 +0200

    Update index.php

--- web/settings/index.php
+++ web/settings/index.php
@@ -89,5 +89,6 @@ else
   { echo "List of hosts is empty."; }
 ?>
     </div>
+    <div id="footer"> PhpWakeOnLan : Dodoslav Novak : <a style="color: rgb(50, 168, 82);" href="https://github.com/dodoslavn/PhpWakeOnLan/">Github</a> </div>
   </body>
 </html>
