a9fedaf3f24d1388b777a9869419f0d9928a7dbc
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 01:26:07 +0200

    test

---
+++ web/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+# start session
+session_start();
+
+# if debug is on, show errors and warnings
+function debug()
+	{
+	if ($_SESSION['debug'])
+		{
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'On');
+		}
+  }
+
+# load config file
+function load_json_config()
+	{
+	$config_file_raw = file_get_contents('./config.json'); 
+	$config = json_decode($config_file_raw); 
+	if (empty($config)) die("failed to parse JSON config");
+	}
+
+# check if logged in
+function check_logged_in()
+	{ if (empty($_SESSION['id'])) header('Location: login/'); }
+
+
+
+?>
--- web/webroot/index.php
+++ web/webroot/index.php
@@ -11,22 +11,12 @@
 
 <div id="content">
 <?php
-session_start();
-$debug = true;
+require '../functions.php'
 
-if ($debug)
-  {
-  error_reporting(E_ALL);
-  ini_set('display_errors', 'On');
-  }
+check_logged_in();
+debug();
+load_json_config();
 
-# load config file
-$config_file_raw = file_get_contents('../config.json'); 
-$config = json_decode($config_file_raw); 
-if (empty($config)) die("failed to parse JSON config");
-
-# check if logged in
-if (empty($_SESSION['id'])) header('Location: login/');
 
 echo '<table>
   <tr>
