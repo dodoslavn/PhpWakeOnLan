8c6b2a03684d49cb0bf76211ce65980d2b9e29c8
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 01:59:07 +0200

    fix

--- web/webroot/settings/index.php
+++ web/webroot/settings/index.php
@@ -1,21 +1,10 @@
 <?
-session_start();
+require '../header.php';
+require '../functions.php';
 
-# debug output switch
-$debug = true;
-if ($debug)
-  {
-  error_reporting(E_ALL);
-  ini_set('display_errors', 'On');
-  }
-
-# load config file
-$config_file_raw = file_get_contents('../../config.json'); 
-$config = json_decode($config_file_raw); 
-if (empty($config)) die("failed to parse JSON config");
-
-# check if logged in
-if (empty($_SESSION['id'])) header('Location: login/');
+check_logged_in();
+debug();
+$config = load_json_config('../');
 
 
 ?>
