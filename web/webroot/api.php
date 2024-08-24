a5f5843bb1d15ebfa65fd73d97a543b4a2a4f400
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 12:35:35 +0200

    fix

--- web/webroot/api.php
+++ web/webroot/api.php
@@ -57,7 +57,8 @@ exec($config->configuration->wol_binary." ".$mac, $output, $retval);
 print_r($output[0]);
 echo $retval;
 
-
+$config->data->$mac->last_used = 'today';
+save_json_config($config);
 
 ?>
 </center>
