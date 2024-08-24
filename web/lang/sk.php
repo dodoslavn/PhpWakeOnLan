e207a70e53fe675fffd36464d898d44fbf488d5f
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 12:55:53 +0200

    fix

---
+++ web/lang/sk.php
@@ -0,0 +1,9 @@
+<?php
+$lang['header_settings']="NASTAVENIA";
+$lang['header_logout']="ODHLASIT";
+
+$lang['settings_youracc']="Vas ucet";
+$lang['settings_accname']="Nazov uctu";
+$lang['settings_accname']="Zmenit heslo";
+
+?>
