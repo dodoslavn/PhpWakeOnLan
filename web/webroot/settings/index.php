99385c5a377d36aaf588419d67344021754abb9d
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 10:57:22 +0200

    fix

--- web/webroot/settings/index.php
+++ web/webroot/settings/index.php
@@ -5,7 +5,7 @@ require '../../functions_settings.php';
 
 check_logged_in();
 debug();
-$config = load_json_config('../');
+global $config = load_json_config('../');
 
 $result;
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
