90f5b0bc67386e6b42a6c0c16bee178e26ea6272
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 12:33:16 +0200

    fix

--- web/webroot/index.php
+++ web/webroot/index.php
@@ -22,6 +22,7 @@ if ( count((array)$config->data) > 0 )
   # loop through data, display ordered records, then the rest
   foreach ( $config->data as $host => $info )
     {
+    if (!isset($info->title)) $info->title = '';
     if ( isset($info->order) )
       { $ordered[$info->order] = '<tr><td>'.$info->title."</td><td>".$host.'<td><a target="_new" href="api.php?title='.$info->title.'">EXECUTE</a></td><td>'.$info->last_used.'</td></tr>'; }
     else 
