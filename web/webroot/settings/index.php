858b6df24da414ec569767ad0c13e1c41a0392c7
Dodoslav Novak <dodoslavn@gmail.com>
2024-08-24 12:13:09 +0200

    fix

--- web/webroot/settings/index.php
+++ web/webroot/settings/index.php
@@ -23,6 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                 }
             else $result = new Result('ERROR: New password too short!', false);
             break;
+        case 'php_debug':
+            $_SESSION['debug'] = !$_SESSION['debug'];
+            break;
         }
     }
 
