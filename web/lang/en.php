f7fc04f1ae831ee6476d8d2aeadc0cfea9f2172e
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-09 15:27:17 +0200

    Update en.php

--- web/lang/en.php
+++ web/lang/en.php
@@ -3,6 +3,9 @@ global $lang;
 $lang['header_settings']="SETTINGS";
 $lang['header_logout']="LOGOUT";
 
+$lang['main_table_title']="TITLE";
+$lang['main_table_lastused']="Last used";
+
 $lang['settings_youracc']="Your account";
 $lang['settings_accname']="Account name";
 $lang['settings_accname']="Change password";
