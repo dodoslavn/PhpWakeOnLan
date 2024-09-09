a7870045a57af44a1d0e38fde7833349a421a76f
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-09 15:43:13 +0200

    Update en.php

--- web/lang/en.php
+++ web/lang/en.php
@@ -5,6 +5,7 @@ $lang['header_logout']="LOGOUT";
 
 $lang['main_table_title']="TITLE";
 $lang['main_table_lastused']="Last used";
+$lang['main_table_exec']="EXECUTE";
 
 $lang['settings_youracc']="Your account";
 $lang['settings_accname']="Account name";
