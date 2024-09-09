031d4a5d544a2d9217357cf1a1fec68b5550ed29
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-09 15:29:21 +0200

    Update sk.php

--- web/lang/sk.php
+++ web/lang/sk.php
@@ -2,6 +2,9 @@
 $lang['header_settings']="NASTAVENIA";
 $lang['header_logout']="ODHLASIT";
 
+$lang['main_table_title']="MENO";
+$lang['main_table_lastused']="Naposledy použité";
+
 $lang['settings_youracc']="Vas ucet";
 $lang['settings_accname']="Nazov uctu";
 $lang['settings_accname']="Zmenit heslo";
